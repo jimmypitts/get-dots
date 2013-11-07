68d8621c96adeb290a7bfff3221f777eb4721ae7
Jimmy Pitts <jimmyrp@gmail.com>
2013-11-06 22:41:01 -0800

    Show correct x and y values

--- js/output.js
+++ js/output.js
@@ -5,7 +5,7 @@ $(document).ready(function() {
         var new_points = [];
         for (var i in points) {
             var point = points[i];
-            new_points.push({x: point[0], y: point[1]});
+            new_points.push({x: point[0], y: (height - point[1])});
         }
 
         $('#output').removeClass('hide').empty().append(JSON.stringify(new_points, undefined, 2));
